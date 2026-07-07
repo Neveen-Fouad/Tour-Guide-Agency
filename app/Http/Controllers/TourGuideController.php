8d5a41ef1d681f037bdfbf34f176e38e4f27571f
sarah-548 <sara70648@gmail.com>
2026-07-08 00:23:43 +0300

    filtering

--- app/Http/Controllers/TourGuideController.php
+++ app/Http/Controllers/TourGuideController.php
@@ -6,9 +6,51 @@ use Illuminate\Http\Request;
 
 class TourGuideController extends Controller
 {
-    public function show($id)
-    {
-        $guide = TourGuide::findOrFail($id);
-        return response()->json($guide);
-    }
+
+
+//guides whose name matches
+public function getByName($name)
+{
+    $guides = TourGuide::where('name', 'like', '%' . $name . '%')->get();
+    return response()->json($guides);
+}
+
+//all guides in a given area
+public function getByArea($area)
+{
+    $guides = TourGuide::where('area', 'like', '%' . $area . '%')->get();
+    return response()->json($guides);
+}
+
+//guides at or under a price
+public function getByPrice($price)
+{
+    $guides = TourGuide::where('price_per_hour', '<=', $price)->get();
+    return response()->json($guides);
+}
+
+//guides who speak a given language
+public function getByLanguage($language)
+{
+    $guides = TourGuide::where('language', 'like', '%' . $language . '%')->get();
+    return response()->json($guides);
+}
+
+
+//Approved Guides Number
+public function approvedCount()
+{
+    $count = TourGuide::where('is_approved', true)->count();
+
+    return response()->json(['No_of_guides' => $count]);
+}
+
+//Pending Guides Number
+public function pendingCount()
+{
+    $count = TourGuide::where('is_approved', false)->count();
+
+    return response()->json(['No_of_pending_guides' => $count]);
+}
+
 }
